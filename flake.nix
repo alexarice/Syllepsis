5d5386d53a4574def2c4f35733120a3712364228
Alex Rice <alexrice999@hotmail.co.uk>
2026-02-20 16:04:05 +0000

    Use old mmd

--- flake.nix
+++ flake.nix
@@ -4,18 +4,18 @@
   inputs = {
     all-agda.url = "github:alexarice/all-agda";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgsold.url = "github:nixos/nixpkgs/8c5d37129fc5097d9fb52e95fb07de75392d1c3c";
   };
 
-  outputs = { self, nixpkgs, all-agda, flake-utils }:
+  outputs = { self, nixpkgsold, all-agda, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
       agdaPackages = all-agda.legacyPackages."x86_64-linux".agdaPackages-2_6_4;
     in {
       packages = rec {
-        page = agdaPackages.callPackage ./page.nix { };
+        page = agdaPackages.callPackage ./page.nix {
+          inherit (nixpkgsold.legacyPackages."${system}") multimarkdown;
+        };
         default = page;
       };
     }
